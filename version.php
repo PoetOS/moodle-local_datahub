31afa050e7451b133c085754e2a10a320888a781
James McQuillan <james.mcquillan@remote-learner.net>
2017-08-02 14:28:22 -0400

    DATAHUB-1625: Update to version 2016120505 release 3.2.2.2

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016120504;
+$plugin->version = 2016120505;
 $plugin->requires = 2016120501;
 $plugin->component = 'local_datahub';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.2.2.1 (Build: 20170514)';
+$plugin->release = '3.2.2.2 (Build: 20170717)';
 $plugin->dependencies = [
     'local_eliscore' => 2016120501
 ];
